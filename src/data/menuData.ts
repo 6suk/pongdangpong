96bf9d8295d475b7871d8435d8e937d005662bff
hyungjin kim <hjux07@gmail.com>
2023-07-27 02:03:06 +0900

    [⚙️chore] menuData 수정

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -1,14 +1,14 @@
 export const subMenuList ={  
   home: [
-    {id:1, content:"홈", path:"/"},
-    {id:2, content: "내 정보 보기" , path:"/me"},
+    {id:"홈", content:"홈", path:"/", hide: false},
+    {id:"내 정보 보기", content: "내 정보 보기", path:"/me" , hide: false},
   ],
   schedule:[
     {id:"수강권 관리", content: "수강권 관리" , path:"/schedule/ticketManagement" , hide:true}, 
     {id:"회원 수정", content: "회원 수정" , path:"/schedule/member" ,  hide: true}, 
   ],
   members:[
-    {id:"기록지", content: "기록지" , path:"/members/record" ,hide: false}, 
+    {id:"기록지", content: "기록지" , path:"/members/record" , hide: false}, 
     {id:"만족도 및 후기", content: "만족도 및 후기" , path:"/members/review" ,hide: false}, 
     {id:"앨범", content: "앨범" , path:"/members/album" , hide: false},     
   ],
@@ -19,7 +19,7 @@ export const subMenuList ={
 
   ],
   me:[
-    {id:"티켓 리스트", content: "티켓 리스트" , path:"/tickets/list"},
-    {id:"티켓 생성", content: "티켓 생성" , path:"/tickets/create"},
+    {id:"티켓 리스트", content: "티켓 리스트" , path:"/tickets/list", hide: false},
+    {id:"티켓 생성", content: "티켓 생성" , path:"/tickets/create", hide: false},
   ],
 }

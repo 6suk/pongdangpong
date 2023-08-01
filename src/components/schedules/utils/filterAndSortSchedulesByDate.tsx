045ee7b77b7174059e9368fa92940f234f0d4882
6suk <koandjo9116@gmail.com>
2023-08-02 06:39:13 +0900

    [✨feat] 일정조회 캘린더 대시보드 기능 구현 (#48)
    - filterAndSortSchedulesByDate로 셀렉트 된 데이터 정렬 및 필터
    - 대시보드 스타일링
    - 대시보드 데이터 매핑

---
+++ src/components/schedules/utils/filterAndSortSchedulesByDate.tsx
@@ -0,0 +1,28 @@
+import { Schedules_list_counseling, Schedules_list_private } from '@apis/schedulesAPIs';
+
+export const filterAndSortSchedulesByDate = (
+  counselingSchedules: Array<Schedules_list_counseling>,
+  privateSchedules: Array<Schedules_list_private>,
+  selectedDate: string
+) => {
+  const allSchedules = [
+    ...counselingSchedules.map(schedule => ({ type: 'counseling', schedule })),
+    ...privateSchedules.map(schedule => ({ type: 'private', schedule })),
+  ];
+
+  return allSchedules
+    .filter(({ schedule }) => {
+      const scheduleDate = new Date(schedule.startAt);
+      const selectedDateforDate = new Date(selectedDate);
+      return (
+        scheduleDate.getFullYear() === selectedDateforDate.getFullYear() &&
+        scheduleDate.getMonth() === selectedDateforDate.getMonth() &&
+        scheduleDate.getDate() === selectedDateforDate.getDate()
+      );
+    })
+    .sort((a, b) => {
+      const dateA = new Date(a.schedule.startAt);
+      const dateB = new Date(b.schedule.startAt);
+      return dateA.getTime() - dateB.getTime();
+    });
+};
---
+++ src/stores/selectedDateSlice.tsx
@@ -0,0 +1,57 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+
+import { Schedules_list_counseling, Schedules_list_private } from '@apis/schedulesAPIs';
+import { CalendarEventType } from '@components/schedules/Calendar';
+import { getCurrentDate } from '@components/schedules/utils/getDate';
+
+const { month: curMonth, year: curYear, date: curDate } = getCurrentDate();
+
+export interface CalendarState {
+  checkDate: string;
+  eventCount: CalendarEventType[];
+  lastNextDates: lastNextDatesState;
+  sortSchedules: ScheduleType[];
+}
+
+interface lastNextDatesState {
+  last: string;
+  next: string;
+}
+
+export type ScheduleType =
+  | { type: string; schedule: Schedules_list_counseling }
+  | { type: string; schedule: Schedules_list_private };
+
+const initialState: CalendarState = {
+  checkDate: `${curYear}-${curMonth < 10 ? '0' + curMonth : curMonth}-${curDate < 10 ? '0' + curDate : curDate}`,
+  eventCount: [{ date: 0, count: 0, month: 0, year: 0 }],
+  lastNextDates: {
+    last: '',
+    next: '',
+  },
+  sortSchedules: [],
+};
+
+export const calendarSlice = createSlice({
+  name: 'selectedDate',
+  initialState,
+  reducers: {
+    setSelectedDate: (state, action: PayloadAction<string>) => {
+      state.checkDate = action.payload;
+    },
+    setEventCount: (state, action: PayloadAction<CalendarEventType[]>) => {
+      state.eventCount = action.payload;
+    },
+    setLastNextDates: (state, action: PayloadAction<lastNextDatesState>) => {
+      state.lastNextDates = action.payload;
+    },
+    setSortSchedules: (state, action: PayloadAction<ScheduleType[]>) => {
+      state.sortSchedules = action.payload;
+    },
+    clearSelectedDate: () => initialState,
+  },
+});
+
+export const { setSelectedDate, clearSelectedDate, setEventCount, setLastNextDates, setSortSchedules } =
+  calendarSlice.actions;
+export const calendarReducer = calendarSlice.reducer;

20a669424022a62e356528bd2093f95d3d9d6819
bang-wol <milk011922@gmail.com>
2023-08-04 18:53:57 +0900

    [✨feat] 센터 정보 조회 페이지 구현

--- src/components/center/info/CenterInfo.tsx
+++ src/components/center/info/CenterInfo.tsx
@@ -1,3 +1,161 @@
+import { useNavigate } from 'react-router-dom';
+
+import { styled } from 'styled-components';
+
+import { MemberIcon, BackIcon } from '@assets/icons/indexIcons';
+import { useSwrData } from '@hooks/apis/useSwrData';
+import theme from '@styles/theme';
+
 export const CenterInfo = () => {
-  return <>센서정보</>;
+  const { data, isLoading } = useSwrData(`me/center`);
+  const { id, name, centerCode, phone, contactLink } = data ?? {};
+  const navigate = useNavigate();
+
+  return (
+    !isLoading && (
+      <>
+        <StaffDetailWrap>
+          <div>
+            <div className="header">
+              <div className="title">
+                <h3>센터 정보</h3>
+              </div>
+              <BackButton onClick={() => navigate(-1)}>
+                <BackIcon />
+                <p>뒤로가기</p>
+              </BackButton>
+            </div>
+            <StaffInfoBar>
+              <div className="infos">
+                <p className="icon-box">
+                  <MemberIcon /> {name}
+                </p>
+              </div>
+              <p className="edit-center">센터 정보 수정이 필요하신가요?</p>
+            </StaffInfoBar>
+          </div>
+          <div>
+            <CenterInfoBox>
+              <div className="table">
+                <div className="table-row">
+                  <p className="title">센터코드</p> <p className="data">{centerCode || '-'}</p>
+                </div>
+                <div className="table-row">
+                  <p className="title">센터 연락처</p> <p className="data">{phone || '-'}</p>
+                </div>
+                <div className="table-row">
+                  <p className="title">카카오 채널 주소</p> <p className="data">{contactLink || '-'}</p>
+                </div>
+              </div>
+            </CenterInfoBox>
+          </div>
+          <div></div>
+        </StaffDetailWrap>
+      </>
+    )
+  );
 };
+
+export const StaffDetailWrap = styled.div`
+  width: 100%;
+  margin-top: 3rem;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  max-width: 1024px;
+
+  .header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+    margin-inline: 1rem;
+    justify-content: space-between;
+
+    .title {
+      display: flex;
+      align-items: center;
+    }
+
+    h3 {
+      font-size: ${theme.font.subTitle};
+      font-weight: 800;
+    }
+  }
+`;
+
+const StaffInfoBar = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  border: 1px solid ${theme.colors.gray[800]};
+  text-align: left;
+  border-radius: 6px;
+  font-size: 16px;
+  justify-content: space-between;
+
+  .infos {
+    display: flex;
+    gap: 3rem;
+    align-items: center;
+  }
+
+  .icon-box {
+    display: flex;
+    align-items: center;
+    gap: 0.6rem;
+    font-weight: 600;
+
+    svg {
+      width: 30px;
+    }
+  }
+
+  .edit-center {
+    font-size: 12px;
+    color: ${theme.colors.gray[400]};
+  }
+`;
+
+export const CenterInfoBox = styled.div`
+  padding: 1rem;
+  border: 1px solid ${theme.colors.gray[800]};
+  border-radius: 6px;
+  font-size: 15px;
+  margin-top: -1rem;
+
+  .table {
+    width: 100%;
+  }
+
+  .table-row {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+
+    .title {
+      width: 150px;
+      text-align: left;
+      color: ${theme.colors.gray[400]};
+    }
+
+    .data {
+      text-align: left;
+      color: ${theme.colors.gray[50]};
+      font-weight: 600;
+    }
+  }
+`;
+
+export const BackButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  svg {
+    width: 6px;
+    fill: ${theme.colors.gray[500]};
+  }
+
+  font-size: ${theme.font.sub};
+  color: ${theme.colors.gray[500]};
+`;

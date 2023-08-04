5c7d086b3b747af275667af4b90a7af59095136e
bang-wol <milk011922@gmail.com>
2023-08-05 01:40:43 +0900

    [✨feat] 마이페이지 내 정보 조회 기능 구현

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -6,59 +6,70 @@ import { MemberIcon, BackIcon } from '@assets/icons/indexIcons';
 import { useSwrData } from '@hooks/apis/useSwrData';
 import theme from '@styles/theme';
 
-export const CenterInfo = () => {
-  const { data, isLoading } = useSwrData(`me/center`);
-  const { id, name, centerCode, phone, contactLink } = data ?? {};
+export const Mypage = () => {
+  const { data, isLoading } = useSwrData(`me`);
+  const { id, type, loginId, name, phone, pwdChangeRequired, center } = data ?? {};
   const navigate = useNavigate();
 
   return (
     !isLoading && (
       <>
-        <CenterInfoWrap>
+        <MyInfoWrap>
           <div>
             <div className="header">
               <div className="title">
-                <h3>센터 정보</h3>
+                <h3>내 정보</h3>
               </div>
               <BackButton onClick={() => navigate(-1)}>
                 <BackIcon />
                 <p>뒤로가기</p>
               </BackButton>
             </div>
-            <CenterInfoBar>
+            <MyInfoBar>
               <div className="infos">
-                <p className="icon-box">
-                  <MemberIcon /> {name}
-                </p>
+                <div className="name-and-role">
+                  <p className="icon-box">
+                    <MemberIcon /> {name}
+                  </p>
+                  <div className="role-box">
+                    {type === 'ADMIN' && <span className="tag">관리자</span>}
+                    <p className="center-name">{center.name}</p>
+                  </div>
+                </div>
               </div>
-              <p className="edit-center">센터 정보 수정이 필요하신가요?</p>
-            </CenterInfoBar>
+              <p className="center-code">
+                센터코드 <span className="data">{center.code}</span>
+              </p>
+            </MyInfoBar>
           </div>
           <div>
-            <CenterInfoBox>
+            <MyInfoBox>
               <div className="table">
                 <div className="table-row">
-                  <p className="title">센터코드</p> <p className="data">{centerCode || '-'}</p>
+                  <p className="title">이름</p> <p className="data">{name || '-'}</p>
                 </div>
                 <div className="table-row">
-                  <p className="title">센터 연락처</p> <p className="data">{phone || '-'}</p>
+                  <p className="title">휴대폰 번호</p> <p className="data">{phone || '-'}</p>
                 </div>
                 <div className="table-row">
-                  <p className="title">카카오 채널 주소</p> <p className="data">{contactLink || '-'}</p>
+                  <p className="title">아이디</p> <p className="data">{loginId || '-'}</p>
+                </div>
+                <div className="table-row">
+                  <p className="title">비밀번호</p> <p className="data">{pwdChangeRequired || '*******'}</p>
                 </div>
               </div>
-            </CenterInfoBox>
+            </MyInfoBox>
           </div>
           <div></div>
-        </CenterInfoWrap>
+        </MyInfoWrap>
       </>
     )
   );
 };
 
-export const CenterInfoWrap = styled.div`
+export const MyInfoWrap = styled.div`
   width: 100%;
-  margin-top: 3rem;
+  margin-top: 1rem;
   display: flex;
   flex-direction: column;
   gap: 3rem;
@@ -83,7 +94,7 @@ export const CenterInfoWrap = styled.div`
   }
 `;
 
-const CenterInfoBar = styled.div`
+const MyInfoBar = styled.div`
   display: flex;
   align-items: center;
   padding: 1rem;
@@ -99,6 +110,26 @@ const CenterInfoBar = styled.div`
     align-items: center;
   }
 
+  .name-and-role {
+    display: flex;
+    gap: 1rem;
+
+    .role-box {
+      display: flex;
+      gap: 0.2rem;
+      align-items: center;
+
+      .tag {
+        font-size: ${theme.font.sm};
+        padding-inline: 0.5rem;
+        padding-block: 0.2rem;
+        background-color: ${theme.colors.pri[900]};
+        color: ${theme.colors.pri[500]};
+        border-radius: 6px;
+      }
+    }
+  }
+
   .icon-box {
     display: flex;
     align-items: center;
@@ -109,14 +140,23 @@ const CenterInfoBar = styled.div`
       width: 30px;
     }
   }
+  .center-name {
+    font-size: 14px;
+    color: ${theme.colors.gray[200]};
+    margin-left: 1rem;
+  }
 
-  .edit-center {
-    font-size: 12px;
-    color: ${theme.colors.gray[400]};
+  .center-code {
+    font-size: 14px;
+    color: ${theme.colors.gray[200]};
+  }
+  .data {
+    font-size: 14px;
+    color: ${theme.colors.pri[600]};
   }
 `;
 
-export const CenterInfoBox = styled.div`
+export const MyInfoBox = styled.div`
   padding: 1rem;
   border: 1px solid ${theme.colors.gray[800]};
   border-radius: 6px;
@@ -159,3 +199,5 @@ export const BackButton = styled.button`
   font-size: ${theme.font.sub};
   color: ${theme.colors.gray[500]};
 `;
+
+export default Mypage;

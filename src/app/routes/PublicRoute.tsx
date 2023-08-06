b1442464e1c70efcdadcacd4dc07d9445c61a1c0
bang-wol <milk011922@gmail.com>
2023-08-06 16:47:47 +0900

    [⚙️chore] 로그인 후 첫 페이지 경로 수정

--- src/app/routes/PublicRoute.tsx
+++ src/app/routes/PublicRoute.tsx
@@ -3,5 +3,5 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { PropsState } from '@/app/App';
 
 export const PublicRoute: React.FC<PropsState> = ({ isLogin }) => {
-  return isLogin ? <Navigate to={'/me'} /> : <Outlet />;
+  return isLogin ? <Navigate to={'/mypage'} /> : <Outlet />;
 };
